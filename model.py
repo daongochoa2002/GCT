269b1319a7064ab05c9a25362e42ba5a5b72a664
Đào Ngọc Hoa <74868806+daongochoa2002@users.noreply.github.com>
2024-06-08 20:10:27 +0700

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from models.GraphEncoder import RGTEncoder, RGCNEncoder
-from models.SequenceEncoder import TransformerEncoder, GRUEncoder
+from models.SequenceEncoder import TransformerEncoder
 import torch_scatter
 from models.ConvTransE import ConvTransE
 
@@ -37,69 +37,93 @@ class TemporalTransformerHawkesGraphModel(nn.Module):
         self.transformer_head_num = config.seqTransformerHeadNum
         self.PAD_TIME = -1
         self.PAD_ENTITY = self.n_ent - 1
+        self.time_span = time_span
+        self.timestep = timestep
+        self.hmax = hmax
 
         self.ent_embeds = nn.Embedding(self.n_ent, self.d_model)
         self.rel_embeds = nn.Embedding(self.n_rel, self.d_model)
         self.graph_encoder = RGTEncoder(self.d_model, self.dropout_rate)
-        #self.graph_encoder = RGCNEncoder(self.d_model, self.n_rel, self.d_model//2, self.dropout_rate)
+        # self.graph_encoder = RGCNEncoder(self.d_model, self.n_rel, self.d_model//2, self.dropout_rate)
         self.seq_encoder = TransformerEncoder(self.d_model, self.d_model, self.transformer_layer_num,
                                               self.transformer_head_num, self.dropout_rate)
-        self.linear_inten_layer = nn.Linear(self.d_model * 3, self.d_model, bias=False)
-        
+
+        self.linear_inten_layer = nn.Linear(self.d_model * 2, self.d_model, bias=False)
+        self.time_inten_layer = nn.Linear(self.d_model * 3, self.d_model, bias=False)
+        self.linear_inten_layer1 = nn.Linear(self.d_model * 2, self.d_model, bias=False)
+
         self.dropout = nn.Dropout(self.dropout_rate)
-        self.Softplus = nn.Softplus(beta=10)
+        self.Softplus = nn.Softplus(beta=0.5)
         self.lp_loss_fn = LabelSmoothingCrossEntropy(eps)
+
+        self.conv2 = torch.nn.Conv1d(2, 50, 3, stride=1, padding=1)
+        self.bn2 = torch.nn.BatchNorm1d(50)
+        self.fc2 = torch.nn.Linear(self.d_model * 50, self.d_model)
+        self.layer_norm = nn.LayerNorm(self.d_model, eps=1e-6)
+
         self.ent_decoder = ConvTransE(self.n_ent,self.d_model, self.dropout_rate, self.dropout_rate, self.dropout_rate)
 
         nn.init.xavier_uniform_(self.ent_embeds.weight)
         nn.init.xavier_uniform_(self.rel_embeds.weight)
         nn.init.xavier_uniform_(self.linear_inten_layer.weight)
-        
+        nn.init.xavier_uniform_(self.time_inten_layer.weight)
+        nn.init.xavier_uniform_(self.linear_inten_layer1.weight)
+        nn.init.xavier_uniform_(self.conv2.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        self.tp_loss_fn = nn.MSELoss()
 
     def forward(self, query_entities, query_relations, history_graphs, history_times, batch_node_ids):
         bs, hist_len = history_times.size(0), history_times.size(1)
-
         history_graphs.ndata['h'] = self.ent_embeds(history_graphs.ndata['id']).view(-1, self.d_model)
         history_graphs.edata['h'] = self.rel_embeds(history_graphs.edata['type']).view(-1, self.d_model)
         history_graphs.edata['qrh'] = self.rel_embeds(history_graphs.edata['query_rel']).view(-1, self.d_model)
         history_graphs.edata['qeh'] = self.ent_embeds(history_graphs.edata['query_ent']).view(-1, self.d_model)
         total_nodes_h = self.graph_encoder(history_graphs)
-
-        query_rel_embeds = self.rel_embeds(query_relations) # [bs, d_model]        
+        query_rel_embeds = self.rel_embeds(query_relations)
         query_ent_embeds = self.ent_embeds(query_entities)
-
         history_gh = total_nodes_h[batch_node_ids].reshape(bs, hist_len, -1)
         history_pad_mask = (history_times == -1).unsqueeze(1)
         local_type = history_graphs.ndata['id'].reshape([bs, -1])
         return query_ent_embeds, query_rel_embeds, history_gh, history_pad_mask, total_nodes_h, local_type
 
-    def entity_prediction(self, query_time, query_ent_embeds, query_rel_embeds,
+    def link_prediction(self, query_time, query_ent_embeds, query_rel_embeds,
                         history_gh, history_times, history_pad_mask, total_nodes_h, local_type):
         bs, hist_len = history_times.size(0), history_times.size(1)
-        seq_query_input = query_rel_embeds.unsqueeze(1)
+        #seq_query_input = query_rel_embeds.unsqueeze(1)
         seq_query_time = query_time.view(-1, 1)  # [bs, 1]
-        
+        res_history_gh = torch.cat([query_rel_embeds.unsqueeze(1).repeat(1,hist_len,1),history_gh],dim=-1)
+        res_history_gh = self.dropout(self.conv2(res_history_gh.reshape(bs,2,-1)))
+        res_history_gh = self.bn2(res_history_gh)
+        res_history_gh = res_history_gh.view(bs,hist_len, -1)
+        history_gh = self.dropout(self.fc2(res_history_gh)) + history_gh
+        history_gh = self.layer_norm(history_gh)
+
+
+        seq_query_input = query_rel_embeds.unsqueeze(1)
+
         output = self.seq_encoder(history_gh, history_times, seq_query_input, seq_query_time, history_pad_mask)
         output = output[:, -1, :]
 
-        inten_raw = self.linear_inten_layer(
-            self.dropout(torch.cat((query_ent_embeds, output, query_rel_embeds), dim=-1)))  # [bs, d_model]
-
+        statics_ent_embeds = self.linear_inten_layer(
+            torch.cat((query_ent_embeds, query_rel_embeds), dim=-1))
+        inten_raw = F.gelu(F.dropout(self.linear_inten_layer1(
+            torch.cat((statics_ent_embeds, output), dim=-1)))) +statics_ent_embeds
+        inten_raw = self.layer_norm(inten_raw)
         #global_intes = inten_raw.mm(self.ent_embeds.weight.transpose(0, 1))  # [bs, ent_num]
         global_type = torch.arange(self.n_ent, device=output.device).unsqueeze(0).repeat(bs, 1)
         global_intes = self.ent_decoder(query_ent_embeds,query_rel_embeds, output,self.ent_embeds.weight)
-        global_intes *= (global_type != self.PAD_ENTITY) + 0.2
-        non_history = torch.zeros_like(global_intes)
-        local_h = total_nodes_h.reshape([bs, -1, self.d_model])  # [bs, max_nodes_num * seq_len, d_model]
-        local_intes = torch.matmul(inten_raw.unsqueeze(1), local_h.transpose(1, 2))[:, -1, :]  # [bs, max_nodes_num * seq_len]
-        history =  torch.ones_like(local_intes)
-        history_tag = torch.cat([non_history, history], dim=-1)
-        local_intes *= (local_type != self.PAD_ENTITY) + 0.2
-        intens = self.Softplus(torch.cat([global_intes, local_intes], dim=-1) + history_tag)
 
-        
+        local_h = total_nodes_h.reshape([bs, -1, self.d_model])
+
+        local_intes = torch.matmul(statics_ent_embeds.unsqueeze(1), local_h.transpose(1, 2))[:, -1, :]\
+                + torch.matmul(inten_raw.unsqueeze(1), local_h.transpose(1, 2))[:, -1, :]  # [bs, max_nodes_num * seq_len]
+
+        intens = self.Softplus(torch.cat([global_intes, local_intes], dim=-1))
+
+
         ent_type = torch.cat([global_type, local_type], dim=-1)
-        return intens, ent_type
+        return intens, ent_type,statics_ent_embeds
+
 
     def link_prediction_loss(self, intens, type, answers):
         intens = torch_scatter.scatter(intens, type, dim=-1, reduce="mean")
@@ -111,27 +135,72 @@ class TemporalTransformerHawkesGraphModel(nn.Module):
         return loss_dt
 
     def ents_score(self, intens, type, local_weight=1.):
-        intens = F.softmax(intens, dim=-1)
-        intens[:, self.n_ent:] = intens[:, self.n_ent:] * 0.8
-        output = torch_scatter.scatter(intens, type, dim=-1, reduce="max")
+        #intens = F.softmax(intens, dim=-1)
+        #intens[:, self.n_ent:] = intens[:, self.n_ent:] * 0.65
+        output = torch_scatter.scatter(intens, type, dim=-1, reduce="mean")
         return output[:, :-1]
+
+    def predict_t(self, tail_ent, dur_last, query_ent_embeds, query_rel_embeds, history_gh, history_times, history_pad_mask,statics_ent_embeds):
+        n_samples = int(self.hmax / self.timestep) + 1  # add 1 to accomodate zero
+        bs, hist_len = history_times.size(0), history_times.size(1)
+        dur_last = dur_last // self.time_span
+        dur_non_zero_idx = (dur_last > 0).nonzero().squeeze(1)
+        dur_last = dur_last[dur_non_zero_idx].type(torch.float)
+        if not dur_last.numel():
+            return torch.tensor([0.]), torch.tensor([0.])
+        dt = torch.linspace(0, self.hmax, n_samples, device=dur_last.device).repeat(dur_last.shape[0], 1)  # [bs, n_sample]
+
+        seq_query_input = query_rel_embeds[dur_non_zero_idx].unsqueeze(1).repeat(1, n_samples, 1)  # [bs , n_sample, d_model]
+        seq_query_time = history_times[dur_non_zero_idx, -1].unsqueeze(1).repeat(1, n_samples) + dt  # [bs, n_sample]
+        sampled_seq_output = self.seq_encoder(history_gh[dur_non_zero_idx], history_times[dur_non_zero_idx],
+                                              seq_query_input, seq_query_time, history_pad_mask[dur_non_zero_idx])  # [bs, n_sample, d_model]
+
+        inten_layer_input = torch.cat((query_ent_embeds[dur_non_zero_idx].unsqueeze(1).repeat(1, n_samples, 1),
+                                    sampled_seq_output, query_rel_embeds[dur_non_zero_idx].unsqueeze(1).repeat(1, n_samples, 1)), dim=-1)
+        inten_raw = self.time_inten_layer(self.dropout(inten_layer_input))  # [bs, n_sample, d_model]
+        o = self.ent_embeds(tail_ent[dur_non_zero_idx]).unsqueeze(1).repeat(1, n_samples, 1)  # [bs, d_model]
+        intensity = self.Softplus((inten_raw * o).sum(dim=2))  # [bs, n_sample]
+
+        integral_ = torch.cumsum(self.timestep * intensity, dim=1)
+        density = (intensity * torch.exp(-integral_))
+        t_pit = dt * density  # [bs, n_sample]
+        estimate_dt = (self.timestep * 0.5 * (t_pit[:, 1:] + t_pit[:, :-1])).sum(dim=1)  # shape: n_batch
+        return estimate_dt, dur_last
 
 
     def train_forward(self, s_ent, relation, o_ent, time, history_graphs, history_times, batch_node_ids):
         query_ent_embeds, query_rel_embeds, history_gh, history_pad_mask, total_nodes_h, local_type = \
             self.forward(s_ent, relation, history_graphs, history_times, batch_node_ids)
-        ent_intes, ent_type = self.entity_prediction(time, query_ent_embeds, query_rel_embeds, history_gh, history_times, history_pad_mask,
+
+        type_intes, type,statics_ent_embeds = self.link_prediction(time, query_ent_embeds, query_rel_embeds, history_gh, history_times, history_pad_mask,
                                                 total_nodes_h, local_type)
-        loss_lp = self.link_prediction_loss(ent_intes, ent_type, o_ent)
-        
-        return loss_lp
+
+        last_time, _ = torch.max(history_times, 1)
+        dur_last = time - last_time
+        dur_last = torch.where(last_time > 0, dur_last, torch.zeros_like(dur_last))
+        estimate_dt, dur_last = self.predict_t(o_ent, dur_last, query_ent_embeds, query_rel_embeds, history_gh, history_times, history_pad_mask,statics_ent_embeds)
+
+        loss_lp = self.link_prediction_loss(type_intes, type, o_ent)
+        loss_tp = self.time_prediction_loss(estimate_dt, dur_last)
+        # loss_tp = 0
+        return loss_lp, loss_tp
+
     def test_forward(self, s_ent, relation, o_ent, time, history_graphs, history_times, batch_node_ids, local_weight=1.):
         query_ent_embeds, query_rel_embeds, history_gh, history_pad_mask, total_nodes_h, local_type = \
             self.forward(s_ent, relation, history_graphs, history_times, batch_node_ids)
 
-        type_intes, ent_type = self.entity_prediction(time, query_ent_embeds, query_rel_embeds, history_gh, history_times,
+        type_intes, type,statics_ent_embeds = self.link_prediction(time, query_ent_embeds, query_rel_embeds, history_gh, history_times,
                                                 history_pad_mask,
                                                 total_nodes_h, local_type)
-        scores = self.ents_score(type_intes, ent_type, local_weight)
 
-        return scores
+        scores = self.ents_score(type_intes, type, local_weight)
+        #
+        last_time, _ = torch.max(history_times, 1)
+        dur_last = time - last_time
+        dur_last = torch.where(last_time > 0, dur_last, torch.zeros_like(dur_last))
+        estimate_dt, dur_last = self.predict_t(o_ent, dur_last, query_ent_embeds, query_rel_embeds, history_gh,
+                                               history_times, history_pad_mask,statics_ent_embeds)
+        # estimate_dt = 0
+        # dur_last = 0
+
+        return scores, estimate_dt, dur_last
